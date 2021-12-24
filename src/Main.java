caae252817937c4d44b0878a570d053214402136
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2021-12-24 14:21:18 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        /*
+        Write a method called isPalindrome with one int parameter called number.
+        The method needs to return a boolean. It should return true if the number is a palindrome number otherwise it
+        should return false. Check the tips below for more info about palindromes.
+        A palindrome number is a number which when reversed is equal to the original number.
+         */
+        System.out.println(NumberPalindrome.isPalindrome(1221));
+    }
+}
---
+++ src/NumberPalindrome.java
@@ -0,0 +1,19 @@
+public class NumberPalindrome {
+    public static boolean isPalindrome(int number){
+        if (number < 0){
+            number *= -1;
+        }
+
+        int reverse = 0;
+        int num = number;
+        while (num > 0) {
+            int lastDigit = num % 10;
+            reverse = (reverse * 10) + lastDigit;
+            num = num / 10;
+        }if (number == reverse){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
